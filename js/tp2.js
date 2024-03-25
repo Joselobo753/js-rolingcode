f05cbd94d08468d70b4d188971ce1985057005d6
Jose Lobo <j.a.lobo753@gmail.com>
2024-03-25 17:48:05 -0300

    finalizacion de js del tp2

--- js/tp2.js
+++ js/tp2.js
@@ -197,7 +197,7 @@ function eje9() {
 escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.*/
 function eje10() {
   alert(
-    "Acontiacion se le pediran filas y columnas para crear una tabla con numeros decendetes"
+    "Acontinuacion se le pediran filas y columnas para crear una tabla con numeros decendetes"
   );
   let filas = prompt("ingrese el numero de filas");
   filas = parseInt(filas);
@@ -210,18 +210,120 @@ function eje10() {
       document.write(numero);
       numero--;
     }
-    document.write("</br>")
+    document.write("</br>");
   }
 }
-/* 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. *
-/*
-12- Realiza un script que genere un número aleatorio entre 1 y 99 */
-function eje12(){
-  alert("su numero aleatorio entre 1 y 99 es "+ parseInt(Math.random(0,99)*100))
+/* 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor.
+ */
+function eje11() {
+  alert("A continuacion se le pediran 3 nombres y las edades correspondientes");
+  let nombre1 = prompt("ingrese el primer nombre");
+  let edad1 = parseInt(prompt("ingrese la edad correspondiente"));
+  let nombre2 = prompt("ingrese el segundo nombre");
+  let edad2 = parseInt(prompt("ingrese la edad correspondiente"));
+  let nombre3 = prompt("ingrese el tercer nombre");
+  let edad3 = parseInt(prompt("ingrese la edad correspondiente"));
+  let mayor = Math.max(edad1, edad2, edad3);
+  if (mayor === edad1) {
+    alert(`El mayor es ${nombre1} con ${edad1} años`);
+  } else if (mayor == edad2) {
+    alert(`El mayor es ${nombre2} con ${edad2} años`);
+  } else if (mayor === edad3) {
+    alert(`El mayor es ${nombre3} con ${edad3} años`);
+  } else {
+    alert("error");
+  }
 }
-/*13- Realiza un script que pida un texto y lo muestre en mayúsculas.
-14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
-15- Realiza un script que cuente el número de vocales que tiene un texto.
-16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
-17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
-*/
+/*12- Realiza un script que genere un número aleatorio entre 1 y 99 */
+function eje12() {
+  alert("su numero aleatorio entre 1 y 99 es " + parseInt(Math.random() * 100));
+}
+/*13- Realiza un script que pida un texto y lo muestre en mayúsculas.*/
+function eje13() {
+  let texto = prompt("ingrese un texto");
+  let texMayu = texto.toUpperCase(texto);
+  alert(`su texto en mayusculas es : " ${texMayu} "`);
+}
+/* 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. 
+Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.*/
+function eje14() {
+  let texto = prompt("ingrese un texto");
+  let textGuiones = "";
+  for (let i = 0; i < texto.length; i++) {
+    textGuiones += texto[i];
+    if (i < texto.length - 1) {
+      textGuiones += "-";
+    }
+  }
+  alert(`su texto con guiones es ${textGuiones}`);
+}
+/*
+15- Realiza un script que cuente el número de vocales que tiene un texto.*/
+function eje15() {
+  let texto = prompt("ingrese un texto");
+  let contarVoc = 0;
+  for (let i = 0; i < texto.length; i++) {
+    switch (texto[i]) {
+      case "a":
+        contarVoc++;
+        break;
+      case "e":
+        contarVoc++;
+        break;
+      case "i":
+        contarVoc++;
+        break;
+      case "o":
+        contarVoc++;
+        break;
+      case "u":
+        contarVoc++;
+        break;
+
+      default:
+        break;
+    }
+  }
+  alert(`su texto tenia ${contarVoc} vocales`);
+}
+
+/*
+16- Realiza un script que pida una cadena de texto y la devuelva al revés.
+ Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”. */
+function eje16() {
+  let texto = prompt("ingrese un texto");
+  let invTexto = "";
+  for (let i = texto.length - 1; i >= 0; i--) {
+    invTexto += texto[i];
+  }
+  alert(`su texto al revez es: "${invTexto}" `);
+}
+
+/* 17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
+ */
+function eje17() {
+  let texto = prompt("ingrese un texto");
+
+  for (let i = 0; i < texto.length; i++) {
+    switch (texto[i]) {
+      case "a":
+        alert(`la vocal ${texto[i]} se encuentra en la posicion ${i + 1}`);
+        break;
+      case "e":
+        alert(`la vocal ${texto[i]} se encuentra en la posicion ${i + 1}`);
+        break;
+      case "i":
+        alert(`la vocal ${texto[i]} se encuentra en la posicion ${i + 1}`);
+        break;
+      case "o":
+        alert(`la vocal ${texto[i]} se encuentra en la posicion ${i + 1}`);
+        break;
+      case "u":
+        alert(`la vocal ${texto[i]} se encuentra en la posicion ${i + 1}`);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
